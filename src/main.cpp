d801c4baaa4d88cd0fd558883c89a4bed872402f
Homero <homero.sanchez-moreira@etu.umontpellier.fr>
2025-03-16 12:59:17 +0100

    add function verif_regle_1

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,17 @@ std::vector<std::vector<int>>  init_grille (int size) {
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
     return matrice;
 }
 
-//verification des colones
+//verification des ligne en premier
+// [[Rcpp::export]]
 std::vector<std::vector<int>>  verif_regle_1 (std::vector<std::vector<int>> matrix) {
     int j = 0 ;
     for (int i = 0; i < matrix.size(); i++) {
-        if (i>2) {
+        if (i>1) {
+            //std::cout <<"On rentre dans le if(j>2) pour la colone"<<i<<std::endl;
             if (matrix[i][j]+matrix[i-1][j]+matrix[i-2][j] == 0) {
                 matrix[i][j] = 1;
             }
@@ -37,5 +40,76 @@ std::vector<std::vector<int>>  verif_regle_1 (std::vector<std::vector<int>> matr
                 matrix[i][j] = 0;
             }
         }
+        for (int j = 0; j < matrix[i].size(); j++) {
+            if (j>1) {
+                //std::cout <<"On rentre dans le if(j>2) pour la ligne"<<j<<std::endl;
+                if (matrix[i][j]+matrix[i][j-1]+matrix[i][j-2] == 0) {
+                    matrix[i][j] = 1;
+                }
+                if (matrix[i][j]+matrix[i][j-1]+matrix[i][j-2] == 3) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+    }
+    return matrix;
+}
+
+//verification des colone en premier
+// [[Rcpp::export]]
+std::vector<std::vector<int>>  verif_regle_1_version2 (std::vector<std::vector<int>> matrix) {
+    int i = 0 ;
+    for (int j = 0; j < matrix.size(); j++) {
+        if (j>1) {
+            //std::cout <<"On rentre dans le if(j>2) pour la colone"<<i<<std::endl;
+            if (matrix[i][j]+matrix[i][j-1]+matrix[i][j-2] == 0) {
+                matrix[i][j] = 1;
+            }
+            if (matrix[i][j]+matrix[i][j-1]+matrix[i][j-1] == 3) {
+                matrix[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < matrix.size(); i++) {
+            if (i>1) {
+                //std::cout <<"On rentre dans le if(j>2) pour la ligne"<<j<<std::endl;
+                if (matrix[i][j]+matrix[i-1][j]+matrix[i-1][j] == 0) {
+                    matrix[i][j] = 1;
+                }
+                if (matrix[i][j]+matrix[i-1][j]+matrix[i-2][j] == 3) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+    }
+    return matrix;
+}
+
+/*
+std::vector<std::vector<int>>  verif_regle_1_colones (std::vector<std::vector<int>> matrix, int j) {
+    for (int i = 0; i < matrix.size(); i++) {
+        if (i>2) {
+            if (matrix[i][j]+matrix[i-1][j]+matrix[i-2][j] == 0) {
+                matrix[i][j] = 1;
+                verif_regle_1_ligne(matrix, i);
+            }
+            if (matrix[i][j]+matrix[i-1][j]+matrix[i-2][j] == 3) {
+                matrix[i][j] = 0;
+                verif_regle_1_ligne(matrix, i);
+            }
+        }
+
+}
+*/
+
+// [[Rcpp::export]]
+void display_matrix(std::vector<std::vector<int>> matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
     }
 }
+
